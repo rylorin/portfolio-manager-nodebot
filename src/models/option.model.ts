acb7d0d8115208b23807621661bed5ce703b4e1d
Ronan-Yann Lorin <ryl@free.fr>
2022-08-05 13:21:25 +0200

    option dte trying to get same result as IB

--- src/models/option.model.ts
+++ src/models/option.model.ts
@@ -63,7 +63,7 @@ export class Option extends Model {
   public theta: number;
 
   get dte(): number {
-    const dte: number = Math.floor(0.5 + (((new Date(this.getDataValue("lastTradeDate"))).getTime() - Date.now()) / 1000 / 86400));
+    const dte: number = Math.floor(0.9 + (((new Date(this.getDataValue("lastTradeDate"))).getTime() - Date.now()) / 1000 / 86400));
     console.log("dte for", this.getDataValue("lastTradeDate"), dte);
     return dte;
   }
